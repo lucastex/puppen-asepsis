f076a973b60216049046a9d255b0c2bfae8fc68c
Lucas Frare Teixeira <lucastex@gmail.com>
2014-03-20 09:04:40 -0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,11 @@
 # Usage:
 #
 #   include asepsis
-class asepsis {
-  $version = '1.4'
+class asepsis (
+  $version = '1.4',
+) {
   package { 'Asepsis':
-    source   => 'http://downloads.binaryage.com/Asepsis-1.4.dmg',
     provider => 'appdmg',
+    source   => 'http://downloads.binaryage.com/Asepsis-1.4.dmg'
   }
 }
-
